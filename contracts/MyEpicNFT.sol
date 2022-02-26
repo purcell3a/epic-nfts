4401b8f4d1b9b91507e19533a9a19b1ba29031c0
Angela Purcell <kalmpurcell@gmail.com>
2022-02-26 12:30:12 -0800

    section 2.3

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -28,7 +28,7 @@ contract MyEpicNFT is ERC721URIStorage {
     _safeMint(msg.sender, newItemId);
 
     // Set the NFTs data.
-    _setTokenURI(newItemId, "https://jsonkeeper.com/b/1KZV");
+    _setTokenURI(newItemId, "https://jsonkeeper.com/b/UWR0");
     console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
 
     // Increment the counter for when the next NFT is minted.
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,19 +3,19 @@ const main = async () => {
     const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
     console.log("Contract deployed to:", nftContract.address);
-  
+
     // Call the function.
     let txn = await nftContract.makeAnEpicNFT()
     // Wait for it to be mined.
     await txn.wait()
     console.log("Minted NFT #1")
-  
+
     txn = await nftContract.makeAnEpicNFT()
     // Wait for it to be mined.
     await txn.wait()
     console.log("Minted NFT #2")
   };
-  
+
   const runMain = async () => {
     try {
       await main();
@@ -25,5 +25,5 @@ const main = async () => {
       process.exit(1);
     }
   };
-  
+
   runMain();
